b37d955b9f9a331c14f4d22a5b5e96b0d5ee5c8b
belfaux <anton.gumiran@gmail.com>
2018-10-10 12:09:37 +0800

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=bfxsdk3webappbotforms-Web-Deploy.pubxml -p:Password=S99zMjEXHbaBPK9B67fqKrYWadkqDK5FT4Kq1bu3F3vWrAXwkYhibamshXNF
+
